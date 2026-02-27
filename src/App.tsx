d9e2284da9f120888cd9a655dd322988c7e673a6
Ana-Dare <analauradare467@gmail.com>
2026-02-27 08:37:39 -0300

    feat add conection socket

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,40 @@ import { GlobalStyle } from "./style/reset";
 import Home from "./screen/home";
 import { OrderProvider } from "./context/OrderContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import { socket } from "./socket";
 
 const queryClient = new QueryClient();
 
 function App() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [fooEvents, setFooEvents] = useState<unknown>([]);
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+      console.log("A conexão foi estabelecida");
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+      console.log("A conexão foi desconectada");
+    }
+
+    function onFooEvent(value: unknown) {
+      setFooEvents((previous: any) => [...previous, value]);
+    }
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+    socket.on("foo", onFooEvent);
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      socket.off("foo", onFooEvent);
+    };
+  }, []);
   return (
     <>
       <QueryClientProvider client={queryClient}>
---
+++ src/socket.ts
@@ -0,0 +1,6 @@
+import { io } from "socket.io-client";
+
+const URL =
+  import.meta.env.MODE === "production" ? undefined : "http://localhost:5173";
+
+export const socket = io(URL);
